ee9488349dbe4454b50cb7be25e2db813127b045
DaiSuke <feng.daisuke@gmail.com>
2017-04-17 23:11:29 +0800

    IO

--- JavaBaseDataType/src/com/Object/IO/FileUtil.java
+++ JavaBaseDataType/src/com/Object/IO/FileUtil.java
@@ -1,7 +1,13 @@
 package com.Object.IO;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
+import java.util.List;
 import java.util.Scanner;
 
 public class FileUtil {
@@ -25,4 +31,68 @@ public class FileUtil {
 			// TODO: handle exception
 		}
 	}
+	
+	
+	// 将对象写入文件中
+	public static void writeObject(Object object, String filePath) {
+		if (object == null) return;
+		if (filePath.length() == 0l) return;
+		FileOutputStream fileOutputStream = null;
+		ObjectOutputStream objectOutputStream = null;
+		try {
+			fileOutputStream = new FileOutputStream(filePath, true);
+			objectOutputStream = new ObjectOutputStream(fileOutputStream);
+			objectOutputStream.writeObject(object);
+			objectOutputStream.flush();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				fileOutputStream.close();
+				objectOutputStream.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	// 读取文件中的对象
+	public static Object readObject(String filePath) {
+		
+		FileInputStream fileInputStream = null;
+		ObjectInputStream objectInputStream = null;
+		try {
+			fileInputStream = new FileInputStream(filePath);
+			objectInputStream = new ObjectInputStream(fileInputStream);
+			return objectInputStream.readObject();
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				fileInputStream.close();
+				objectInputStream.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return null;
+	}
+	
+	
+	
+	
 }
